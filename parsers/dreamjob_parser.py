5a795d74965f8bf1d171838f7719e3214fe41f15
Ivan Ermolenko <ivanermolenko61@gmail.com>
2026-09-23 16:14:57 +0300

    Increase DreamJob max_pages to 5

--- parsers/dreamjob_parser.py
+++ parsers/dreamjob_parser.py
@@ -99,7 +99,7 @@ class DreamJobParser(BaseParser):
 
     VACANCY_HREF_PATTERN = re.compile(r"/employers/\d+/vakansii/(\d+)")
 
-    def __init__(self, max_pages: int = 2):
+    def __init__(self, max_pages: int = 5):
         self.max_pages = max_pages
         self.headers = {
             "User-Agent": (
